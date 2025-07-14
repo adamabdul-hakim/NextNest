1aa0a09b0cca76122966ea16537589a5d243f99d
Miles1744 <mithomtab@gmail.com>
2025-07-14 17:18:01 -0400

    New

--- frontend/NextNest/src/App.jsx
+++ frontend/NextNest/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './App.css'
 
 function App() {
+  const navigate = useNavigate();
 
+  const handleStart = () => {
+    navigate('/InputForm');
+  };
 
   return (
+
     <>
       <h1>Welcome to NextNest</h1>
       <h5>Explore what life would be like in another state</h5>
-      <button>Get Started</button>
+      <button onClick={handleStart}>Get Started</button>
     </>
   )
 }
--- frontend/NextNest/src/main.jsx
+++ frontend/NextNest/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { StrictMode } from 'react';
+import App from './App.jsx';
+import InputForm from './InputForm.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}/>
+          <Route path="/InputForm" element={<InputForm />}/>
+        </Routes>
+      </BrowserRouter>
+  </StrictMode>
 );
