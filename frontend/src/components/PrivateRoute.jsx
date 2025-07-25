d9c322197fb1138ca6a4416c676d4e9402af34ea
aakim_9 <adama@berea.edu>
2025-07-25 12:20:33 -0400

    correct loading state and adjust PrivateRoute redirect

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,17 +1,14 @@
-
 import { UserAuth } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({children}) => {
-    const {session} = UserAuth();
+const PrivateRoute = ({ children }) => {
+  const { session } = UserAuth();
 
-    if(session === undefined){
-        return(
-            <div></div>
-        )
-    }
+  if (session === undefined) {
+    return <div></div>;
+  }
 
-    return <>{session ? <>{children}</> : <Navigate to="/signup" />}</>;
+  return <>{session ? <>{children}</> : <Navigate to="/signin" />}</>;
 };
 
 export default PrivateRoute;
